f0fa1bc7fb9a9beef9dd48889456c4128226db4a
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2021-12-27 19:24:11 -0500

    Update main.js

--- trianglecalc/main.js
+++ trianglecalc/main.js
@@ -1,13 +1,13 @@
 function findPage(side1, side2, side3, angle1, angle2, angle3) {
-    if(side1 && side2 && side3 && angle1 && angle2 && angle3){
+    if (side1 && side2 && side3 && angle1 && angle2 && angle3) {
         window.location.href = "./Solved/"
-    }else if((side1 && side2 && angle3) || (side2 && side3 && angle1) || (side3 && side1 && angle2)){
+    } else if ((side1 && side2 && angle3) || (side2 && side3 && angle1) || (side3 && side1 && angle2)) {
         window.location.href = "./SAS/"
-    }else if((angle1 && angle2 && side3) || (angle2 && angle3 && side1) || (angle1 && angle3 && side2)){
+    } else if ((angle1 && angle2 && side3) || (angle2 && angle3 && side1) || (angle1 && angle3 && side2)) {
         window.location.href = "./ASA/"
-    }else if(side1 && side2 && side3){
+    } else if (side1 && side2 && side3) {
         window.location.href = "./SSS/"
-    }else if(angle1 && angle2 && angle3){
+    } else if (angle1 && angle2 && angle3) {
         window.location.href = "./AAA/"
     }
 }
