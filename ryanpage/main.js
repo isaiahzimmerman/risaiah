bb6c6567de9542f457642b8e7ff5f5d276bed795
ryanwmerrick <84752262+ryanwmerrick@users.noreply.github.com>
2022-01-12 19:08:03 -0500

    Update main.js

--- ryanpage/main.js
+++ ryanpage/main.js
@@ -1,2 +1,13 @@
-//let canvas= document.getElementby
-return 'hello'
+const canvas= document.getElementById(ryancanvas);
+const ctx = canvas.getContext('2d');
+function drawLine(x1,y1,x2,y2){
+  ctx.beginPath();
+  ctx.strokeStyle= 'purple';
+  ctx.moveTo(x1,y1);
+  ctx.lineTo(x2,y2);
+  ctx.stroke();
+}
+
+drawLine(50,50,50,350);
+drawLine(150,50,150,350);
+drawLine(50,100,150,100);
