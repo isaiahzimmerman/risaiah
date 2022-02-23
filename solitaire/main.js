bd8581aacbb5aba0e6af12516ba0a04d5441e3b3
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2022-02-22 23:00:55 -0500

    fixrd solitaire

--- solitaire/main.js
+++ solitaire/main.js
@@ -1,5 +1,7 @@
 selected = [false]
 
+currentDrawnCard = -1
+
 solvePile = [['',0],['',0],['',0],['',0]]
 
 const deckOfCards = []
@@ -81,6 +83,17 @@ function randomCard(){
     return [suit, (Math.randInt(13)+1), false, isRed]
 }
 
+board = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+]
+
 function addCard(card){
     if(card[0] == 'diamonds' || card[0] == 'hearts'){
         isRed = true
@@ -91,95 +104,70 @@ function addCard(card){
     return [card[0], card[1], false, isRed]
 }
 
-board = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-]
+drawPile = []
 
 function createBoard(){
-    board[0] = []
-    board[0].push(randomCard())
-    board[1] = []
-    board[1].push(randomCard())
-    board[1].push(randomCard())
-    board[2] = []
-    board[2].push(randomCard())
-    board[2].push(randomCard())
-    board[2].push(randomCard())
-    board[3] = []
-    board[3].push(randomCard())
-    board[3].push(randomCard())
-    board[3].push(randomCard())
-    board[3].push(randomCard())
-    board[4] = []
-    board[4].push(randomCard())
-    board[4].push(randomCard())
-    board[4].push(randomCard())
-    board[4].push(randomCard())
-    board[4].push(randomCard())
-    board[5] = []
-    board[5].push(randomCard())
-    board[5].push(randomCard())
-    board[5].push(randomCard())
-    board[5].push(randomCard())
-    board[5].push(randomCard())
-    board[5].push(randomCard())
-    board[6] = []
-    board[6].push(randomCard())
-    board[6].push(randomCard())
-    board[6].push(randomCard())
-    board[6].push(randomCard())
-    board[6].push(randomCard())
-    board[6].push(randomCard())
-    board[6].push(randomCard())
+    cardNumber = 0
+    for(let i = 0; i<7; i++){
+        column = i
+        for(let i = 0; i<column+1; i++){
+            board[column].push(addCard(shuffledDeck[cardNumber]))
+            cardNumber++
+        }
+    }
+
+    for(let i = 0; i<24; i++){
+        drawPile.push(shuffledDeck[cardNumber])
+        cardNumber++
+    }
 
     drawBoard()
 }
 
 function drawBoard(){
     board.forEach(function (item, index) {
-        newValue = ''
-        topIndex = index
-        item.forEach(function (item, index){
-            //flips top card
-            if(item != undefined){
-                if(!item[2] && (board[topIndex].length == (index+1))){
-                    item[2] = true
-                }
+        if(index<7){
+            newValue = ''
+            topIndex = index
+            item.forEach(function (item, index){
+                //flips top card
+                if(item != undefined){
+                    if(!item[2] && (board[topIndex].length == (index+1))){
+                        item[2] = true
+                    }
 
-                //determines if card face is visible
-                if(item[2]){
-                    source = "./cards/"+item[0]+'_'+item[1]+".svg"
-                }else{
-                    source = "./cards/back.svg"
-                }
+                    //determines if card face is visible
+                    if(item[2]){
+                        source = "./cards/"+item[0]+'_'+item[1]+".svg"
+                    }else{
+                        source = "./cards/back.svg"
+                    }
 
-                newValue += "<img class='card' onclick='select("+topIndex+","+index+")' id='play"+topIndex+"_"+index+"' src="+source+">"
-            }
-        })
-        document.getElementById('play'+index).innerHTML = newValue
-        if(item[0] == undefined){
-            document.getElementById('play'+index).innerHTML = "<img class='card' onclick='select("+index+",0)' id='play"+index+"_0' src='./cards/blank.svg'>"
-        }
-        
-        item.forEach(function (item, index){
-            if(item!=undefined){
-                document.getElementById('play'+topIndex+"_"+index).style.top = (index*18 + '%')
+                    newValue += "<img class='card' onclick='select("+topIndex+","+index+")' id='play"+topIndex+"_"+index+"' src="+source+">"
+                }
+            })
+            document.getElementById('play'+index).innerHTML = newValue
+            if(item[0] == undefined){
+                document.getElementById('play'+index).innerHTML = "<img class='card' onclick='select("+index+",0)' id='play"+index+"_0' src='./cards/blank.svg'>"
             }
             
-        })
-        
-
+            item.forEach(function (item, index){
+                if(item!=undefined){
+                    document.getElementById('play'+topIndex+"_"+index).style.top = (index*18 + '%')
+                }
+                
+            })}
     });
+    if(currentDrawnCard>=0){
+        document.getElementById('deck1').src = "./cards/"+drawPile[currentDrawnCard][0]+"_"+drawPile[currentDrawnCard][1]+'.svg'
+        board[7][0] = addCard([drawPile[currentDrawnCard][0],drawPile[currentDrawnCard][1]])
+    }else{
+        document.getElementById('deck1').src = "./cards/blank.svg"
+    }
 }
 
 function isValid(column1, row1, column2, row2) {
-    if(!((board[column2][row2][1] + 1 == board[column1][row1][1]) || (board[column1][row1][3] == board[column2][row2][3]))){
+    if((!(board[column2][row2][1] + 1 == board[column1][row1][1]) || (board[column1][row1][3] == board[column2][row2][3]))){
         return false
     }
     return true
@@ -198,16 +186,23 @@ function validStack(column, startPos){
     return true
 }
 
-function moveStack(move1, move2, target1, target2){
-    cardsToMove = board[move1].length - move2
-    for (let i = 0; i < cardsToMove; i++) {
-        board[target1].push(board[move1][move2+i])
-    }
-    for (let i = 0; i < cardsToMove; i++) {
-        board[move1].pop()
-    }
+function moveStack(move1, move2, target1){
+
+        cardsToMove = board[move1].length - move2
+        for (let i = 0; i < cardsToMove; i++) {
+            board[target1].push(board[move1][move2+i])
+        }
+        for (let i = 0; i < cardsToMove; i++) {
+            board[move1].pop()
+        }
+
+        if(selected[1] == 7){
+            drawPile.splice(currentDrawnCard, 1)
+            currentDrawnCard-=1
+        }
+        console.log('ffffffds')
+        drawBoard()
     
-    drawBoard()
 }
 
 function select(index1, index2){
@@ -215,14 +210,14 @@ function select(index1, index2){
         console.log('j')
         if(validStack(index1, index2)){
             console.log('i')
-            if(board[index1][board[index1].length - 1] == undefined){
-                if(board[selected[1]][board[selected[1]].length - 1][1]==13){
+            if(board[index1][index2] == undefined){
+                if(board[selected[1]][selected[2]][1] == 13){
                     console.log('king to blanks')
-                    moveStack(selected[1], selected[2], index1, index2)
+                    moveStack(selected[1], selected[2], index1)
                 }
                 
-            }else if(isValid(index1, index2, [selected[1]], [selected[2]]) && board[index1][index2][1] == board[selected[1]][selected[2]][1]+1){
-                moveStack(selected[1], selected[2], index1, index2)
+            }else if(isValid(index1, index2, selected[1], selected[2]) && (board[index1][board[index1].length-1][1] == board[selected[1]][selected[2]][1]+1)){
+                moveStack(selected[1], selected[2], index1)
             }
         }
         selected[0] = !selected[0]
@@ -235,6 +230,36 @@ function select(index1, index2){
     }
 }
 
+function drawCard(){
+    currentDrawnCard++
+
+    if(currentDrawnCard == drawPile.length){
+        currentDrawnCard = -1
+    }
+
+    if(currentDrawnCard+1 == drawPile.length){
+        document.getElementById('deck0').src = './cards/blank.svg'
+    }else{
+        document.getElementById('deck0').src = './cards/back.svg'
+    }
+
+    if(currentDrawnCard>=0){
+        board[7][0] = addCard([drawPile[currentDrawnCard][0],drawPile[currentDrawnCard][1]])
+    }
+    
+    drawBoard()
+}
+
+function fromDrawPile(){
+    console.log('fff')
+    if(selected[0]){
+    }else{
+        selected = [!selected[0], 7, 0]
+    }
+
+    drawBoard()
+}
+
 function placeOnDeck(deck){
     console.log('SElected: '+board[selected[1]][selected[2]][1]+' target: '+solvePile[deck][1]+1)
     if(selected[0]){
@@ -242,8 +267,15 @@ function placeOnDeck(deck){
             console.log("./cards/"+board[selected[1]][selected[2]][0]+"_"+board[selected[1]][selected[2]][1])
             document.getElementById('pile'+deck).src = "./cards/"+board[selected[1]][selected[2]][0]+"_"+board[selected[1]][selected[2]][1]+'.svg'
             board[selected[1]].pop()
+            solvePile[deck][1]++
             selected[0] = !selected[0]
-            drawBoard()
         }
+
+        if(selected[1] == 7){
+            drawPile.splice(currentDrawnCard, 1)
+            currentDrawnCard-=1
+        }
+
+        drawBoard()
     }
 }
