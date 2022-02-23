c53d2fc8888ac2adce06bee7b22d7f636b233621
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2022-02-22 20:38:43 -0500

    d

--- solitaire/main.js
+++ solitaire/main.js
@@ -1,10 +1,50 @@
 selected = [false]
 
-function drawStack() {}
+solvePile = [['',0],['',0],['',0],['',0]]
 
-const deckOfCards = [
-    []
-]
+const deckOfCards = []
+
+for (let i = 0; i < 4; i++){
+    suitIndex = i
+    for (let i = 0; i < 13; i++){
+        switch(suitIndex){
+            case 0:
+                suit = 'diamonds'
+                break;
+            case 1:
+                suit = 'hearts'
+                break;
+            case 2:
+                suit = 'spades'
+                break;
+            case 3:
+                suit = 'clubs'
+                break;
+        }
+        deckOfCards.push([suit, i+1])
+    }
+}
+
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
+shuffledDeck = shuffle(deckOfCards)
 
 Math.randInt = function(max) {
     return Math.floor(Math.random()*max)
@@ -31,36 +71,37 @@ function randomCard(){
             break;
 
     }
-    return [suit, (Math.randInt(13)+1), false]
+
+    if(suit == 'diamonds' || suit == 'hearts'){
+        isRed = true
+    }else{
+        isRed = false
+    }
+
+    return [suit, (Math.randInt(13)+1), false, isRed]
+}
+
+function addCard(card){
+    if(card[0] == 'diamonds' || card[0] == 'hearts'){
+        isRed = true
+    }else{
+        isRed = false
+    }
+
+    return [card[0], card[1], false, isRed]
 }
 
 board = [
-    [
-        ['diamonds', 1, false],
-        ['clubs', 2, false],
-        ['hearts', 13, true]
-    ],
-    [
-        ['diamonds', 1, true]
-    ],
-    [
-        ['diamonds', 1, false]
-    ],
-    [
-        ['diamonds', 1, false]
-    ],
-    [
-        ['diamonds', 1, true]
-    ],
-    [
-        ['diamonds', 1, false]
-    ],
-    [
-        ['diamonds', 1, false]
-    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
 ]
 
-function randomBoard(){
+function createBoard(){
     board[0] = []
     board[0].push(randomCard())
     board[1] = []
@@ -137,24 +178,54 @@ function drawBoard(){
     });
 }
 
+function isValid(column1, row1, column2, row2) {
+    if(!((board[column2][row2][1] + 1 == board[column1][row1][1]) || (board[column1][row1][3] == board[column2][row2][3]))){
+        return false
+    }
+    return true
+}
+
+function validStack(column, startPos){
+    checks = board[column].length - startPos - 1
+
+    for (let i = 0; i < checks; i++) {
+        console.log(board[column][i+startPos][0])
+        if(!isValid(column+i, startPos+i, column+i+1, startPos+i+1)){
+            return false
+        }
+    }
+
+    return true
+}
+
 function moveStack(move1, move2, target1, target2){
-    board[target1].push(board[move1][move2])
-    board[move1].pop()
+    cardsToMove = board[move1].length - move2
+    for (let i = 0; i < cardsToMove; i++) {
+        board[target1].push(board[move1][move2+i])
+    }
+    for (let i = 0; i < cardsToMove; i++) {
+        board[move1].pop()
+    }
+    
     drawBoard()
 }
 
 function select(index1, index2){
-    console.log(board[index1][board[index1].length - 1])
     if(selected[0]){
-        selected[0] = !selected[0]
-        if(board[index1][board[index1].length - 1] == undefined){
-            console.log(board[selected[1]][board[selected[1]].length - 1][1])
-            if(board[selected[1]][board[selected[1]].length - 1][1]==13){
+        console.log('j')
+        if(validStack(index1, index2)){
+            console.log('i')
+            if(board[index1][board[index1].length - 1] == undefined){
+                if(board[selected[1]][board[selected[1]].length - 1][1]==13){
+                    console.log('king to blanks')
+                    moveStack(selected[1], selected[2], index1, index2)
+                }
+                
+            }else if(isValid(index1, index2, [selected[1]], [selected[2]]) && board[index1][index2][1] == board[selected[1]][selected[2]][1]+1){
                 moveStack(selected[1], selected[2], index1, index2)
             }
-        }else{
-            moveStack(selected[1], selected[2], index1, index2)
         }
+        selected[0] = !selected[0]
     }else{
         if(board[index1][board[index1].length - 1] != undefined){
             selected[1] = index1
@@ -162,5 +233,17 @@ function select(index1, index2){
             selected[0] = !selected[0]
         }
     }
-    
+}
+
+function placeOnDeck(deck){
+    console.log('SElected: '+board[selected[1]][selected[2]][1]+' target: '+solvePile[deck][1]+1)
+    if(selected[0]){
+        if(solvePile[deck][1]+1 == board[selected[1]][selected[2]][1]){
+            console.log("./cards/"+board[selected[1]][selected[2]][0]+"_"+board[selected[1]][selected[2]][1])
+            document.getElementById('pile'+deck).src = "./cards/"+board[selected[1]][selected[2]][0]+"_"+board[selected[1]][selected[2]][1]+'.svg'
+            board[selected[1]].pop()
+            selected[0] = !selected[0]
+            drawBoard()
+        }
+    }
 }
