38864791680cff34dfbecf26bcff3f9c2163b13d
ryanwmerrick <ryanmerrick86@gmail.com>
2022-03-02 22:11:52 -0500

    tictactoe

---
+++ tictactoe/main.js
@@ -0,0 +1,59 @@
+
+
+let xturn= true
+let gameOver= false
+let ryan= 'hello'
+
+
+let board= [
+    '','','',
+    '','','',
+    '','','',
+]
+
+if (xturn){
+    // 1
+    document.getElementById('one').onclick=function changeValue(){
+        document.getElementById('one').textContent="X";
+    }
+    // 2
+    document.getElementById('two').onclick=function changeValue(){
+        document.getElementById('two').textContent="X";
+    }
+    // 3
+    document.getElementById('three').onclick=function changeValue(){
+        document.getElementById('three').textContent="X";
+    }
+    // 4
+    document.getElementById('four').onclick=function changeValue(){
+        document.getElementById('four').textContent="X";
+    }
+    // 5
+    document.getElementById('five').onclick=function changeValue(){
+        document.getElementById('five').textContent="X";
+    }
+    // 6
+    document.getElementById('six').onclick=function changeValue(){
+        document.getElementById('six').textContent="X";
+    }
+    // 7
+    document.getElementById('seven').onclick=function changeValue(){
+        document.getElementById('seven').textContent="X";
+    }
+    // 8
+    document.getElementById('eight').onclick=function changeValue(){
+        document.getElementById('eight').textContent="X";
+    }
+    // 9
+    document.getElementById('nine').onclick=function changeValue(){
+        document.getElementById('nine').textContent="X";
+    }
+}
+
+
+// when one box in grid is clicked
+
+
+
+
+
