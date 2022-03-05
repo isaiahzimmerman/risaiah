e17e214a4234c1aad34c3135831fce78889e17ac
ryanwmerrick <ryanmerrick86@gmail.com>
2022-03-05 11:53:14 -0500

    Tic Tac Toe Working V1

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -2,58 +2,214 @@
 
 let xturn= true
 let gameOver= false
-let ryan= 'hello'
 
 
+let winner= document.getElementById("winner")
 let board= [
     '','','',
     '','','',
     '','','',
 ]
 
-if (xturn){
-    // 1
-    document.getElementById('one').onclick=function changeValue(){
-        document.getElementById('one').textContent="X";
+var boxZero= document.getElementById("box0")
+var boxOne= document.getElementById("box1")
+var boxTwo= document.getElementById("box2")
+var boxThree= document.getElementById("box3")
+var boxFour= document.getElementById("box4")
+var boxFive= document.getElementById("box5")
+var boxSix= document.getElementById("box6")
+var boxSeven= document.getElementById("box7")
+var boxEight= document.getElementById("box8")
+
+
+function winCheck(){
+    //Top Row Horizontal
+    if((board[0]===board[1] && board[0]===board[2])){
+        if(board[0]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxZero.style.color="#B1717D"
+            boxOne.style.color="#B1717D"
+            boxTwo.style.color="#B1717D"
+        }else if(board[0]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxZero.style.color="#B1717D"
+            boxOne.style.color="#B1717D"
+            boxTwo.style.color="#B1717D"
+        }
+        
     }
-    // 2
-    document.getElementById('two').onclick=function changeValue(){
-        document.getElementById('two').textContent="X";
+    //Middle Row Horizontal
+    if((board[3]===board[4] && board[3]===board[5])){
+        if(board[3]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxThree.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxFive.style.color="#B1717D"
+        }else if(board[3]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxThree.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxFive.style.color="#B1717D"
+        }
+
     }
-    // 3
-    document.getElementById('three').onclick=function changeValue(){
-        document.getElementById('three').textContent="X";
+    //Bottom Row Horizontal
+    if((board[6]===board[7] && board[6]===board[8])){
+        if(board[6]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxSix.style.color="#B1717D"
+            boxSeven.style.color="#B1717D"
+            boxEight.style.color="#B1717D"            
+        }else if(board[6]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxSix.style.color="#B1717D"
+            boxSeven.style.color="#B1717D"
+            boxEight.style.color="#B1717D"             
+        }
+
     }
-    // 4
-    document.getElementById('four').onclick=function changeValue(){
-        document.getElementById('four').textContent="X";
+    //Left Row Vertical
+    if((board[0]===board[3] && board[0]===board[6])){
+        if(board[0]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxZero.style.color="#B1717D"
+            boxThree.style.color="#B1717D"
+            boxSix.style.color="#B1717D"             
+        }else if(board[0]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxZero.style.color="#B1717D"
+            boxThree.style.color="#B1717D"
+            boxSix.style.color="#B1717D"    
+        }
+
     }
-    // 5
-    document.getElementById('five').onclick=function changeValue(){
-        document.getElementById('five').textContent="X";
+    //Middle Row Vertical
+    if((board[1]===board[4] && board[1]===board[7])){
+        if(board[1]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxOne.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxSeven.style.color="#B1717D"    
+        }else if(board[1]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxOne.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxSeven.style.color="#B1717D"  
+        }
+
     }
-    // 6
-    document.getElementById('six').onclick=function changeValue(){
-        document.getElementById('six').textContent="X";
+    //Right Row Vertical
+    if((board[2]===board[5] && board[2]===board[8])){
+        if(board[2]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxTwo.style.color="#B1717D"
+            boxFive.style.color="#B1717D"
+            boxEight.style.color="#B1717D"  
+        }else if(board[2]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxTwo.style.color="#B1717D"
+            boxFive.style.color="#B1717D"
+            boxEight.style.color="#B1717D"  
+        }
+
     }
-    // 7
-    document.getElementById('seven').onclick=function changeValue(){
-        document.getElementById('seven').textContent="X";
+    //Top Left - Bottom Right
+    if((board[0]===board[4] && board[0]===board[8])){
+        if(board[0]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxZero.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxEight.style.color="#B1717D"  
+        }else if(board[0]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxZero.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxEight.style.color="#B1717D" 
+        }
+
     }
-    // 8
-    document.getElementById('eight').onclick=function changeValue(){
-        document.getElementById('eight').textContent="X";
+    //Top Right- Bottom Left
+    if((board[2]===board[4] && board[2]===board[6])){
+        if(board[2]==="X"){
+            winner.innerHTML="X Wins!"
+            gameOver=true
+            boxTwo.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxSix.style.color="#B1717D" 
+        }else if(board[2]==="O"){
+            winner.innerHTML="O Wins!"
+            gameOver=true
+            boxTwo.style.color="#B1717D"
+            boxFour.style.color="#B1717D"
+            boxSix.style.color="#B1717D"    
+        }
+
     }
-    // 9
-    document.getElementById('nine').onclick=function changeValue(){
-        document.getElementById('nine').textContent="X";
+   
+}
+function drawBoard(){
+    for (let i=0; i<9; i++){
+        document.getElementById("box" + i).innerHTML= board[i];
+
     }
 }
+function turn(number){
+    let boxnumber= document.getElementById("box"+number)
+    if (boxnumber.innerHTML== "" && gameOver==false){
+
+        if(xturn){
+            board[number]="X";
+        }else {
+            board[number]="O";
+        }
+        xturn =! xturn
+        drawBoard()
+        winCheck()
+    }
+}
+function restart(){
+    if(gameOver==true){
+        //clears board
+        board= [
+            '','','',
+            '','','',
+            '','','',
+        ];
+
+        //resets colors
+        boxZero.style.color="#1A2930"
+        boxOne.style.color="#1A2930"
+        boxTwo.style.color="#1A2930"
+        boxThree.style.color="#1A2930"
+        boxFour.style.color="#1A2930"
+        boxFive.style.color="#1A2930"
+        boxSix.style.color="#1A2930"
+        boxSeven.style.color="#1A2930"
+        boxEight.style.color="#1A2930"
+
+        //winner message remove
+        winner.innerHTML=""
+
+        //restarts game to make it playable
+        gameOver=false
+
+        drawBoard()
+    }
 
 
-// when one box in grid is clicked
 
-
-
-
-
+}
