148d0d7e1fd5eb8ca13e8af6e765d0e05eaffcfb
Isaiah Zimmerman <isaiahzimmerman673@gmail.com>
2022-11-28 10:21:11 -0500

    update main.js

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ function loadSite(){
 
     document.getElementsByClassName("bodyHTML")[0].innerHTML = bodyHTML
     setColorScheme(getPreferredColorScheme())
+    cacheSiteImages();
 }
 
 function setColorScheme(scheme) {
@@ -59,20 +60,53 @@ function setColorScheme(scheme) {
         console.log('default')
         })
     }
-  }
-  
-  function getPreferredColorScheme() {
+}
+    
+function getPreferredColorScheme() {
     if (window.matchMedia) {
-      if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-        return 'dark';
-      } else {
-        return 'light';
-      }
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+            return 'dark';
+        } else {
+            return 'light';
+        }
     }
     return 'light';
-  }
-  
+}
+/*  
   if(window.matchMedia){
     var colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
     colorSchemeQuery.addEventListener('change', setColorScheme(getPreferredColorScheme()));
   }
+}
+*/
+
+function cacheSiteImages(){
+    imgsList = []
+    siteList.forEach(function(element, index){
+        element.icon.forEach(function(element2, index2){
+            imgsList.push(element2);
+        })
+    }) 
+    cacheImages(imgsList)
+}
+
+function cacheImages(array)
+{
+    if (!cacheImages.list) {
+        cacheImages.list = [];
+    }
+    var list = cacheImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
