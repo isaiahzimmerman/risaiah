c48ac4e17ed7d810472cca7ca7a2df2652796716
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2022-11-28 00:51:54 -0500

    retain checked values

--- companeros/main.js
+++ companeros/main.js
@@ -159,9 +159,23 @@ function addName(input){
 }
 
 function removeName(index){
+    checkedVals = []
+    for(i = 0; i<names.length; i++){checkedVals.push([])}
+    names.forEach(function(element2,index2){
+        element2.namesList.forEach(function(element3, index3){
+            checkedVals[index2].push(document.getElementById("absent"+index2+"_"+index3).checked)
+        })
+    })
     names[currentSite].namesList.splice(index, 1)
+    checkedVals[currentSite].splice(index, 1)
     //refreshCookie()
     drawSite()
+    names.forEach(function(element2,index2){
+        element2.namesList.forEach(function(element3, index3){
+            document.getElementById("absent"+index2+"_"+index3).checked = checkedVals[index2][index3]
+        })
+    })
+
 }
 
 function nameButton(){
@@ -173,57 +187,65 @@ function nameButton(){
     })
 }
 
+function createOneGroup(el, ind){
+    var today = new Date()
+    customSeed = parseInt(document.getElementById("customSeed"+ind).value)
+    seed = (isNaN(customSeed) ? today.getDate() : customSeed)
+    namesList = []
+    el.namesList.forEach(function(element, index){
+        if(!document.getElementById('absent'+ind+'_'+index).checked){
+            namesList.push(element)
+        }
+    })
+    namesList = shuffle(namesList, seed)
+    //shuffled = ''
+    size = parseInt((document.getElementById('groupSize'+ind).value))
+    if(size<1){
+        return "<div class='groupItem'>Group size cannot be less than 1!</div>"
+    }
+    if(namesList.length == 0){
+        return "<div class='groupItem'>Please add items to list.</div>"
+    }
+    if(isNaN(size)){
+        size=2
+    }
+    extra = namesList.length%size
+    numGroups = Math.floor(namesList.length/size)
+    if(numGroups<1){numGroups = 1}
+    groupsList=[]
+    for(i=0; i<numGroups; i++){
+        z = 0
+        if(extra/(numGroups-i)>1){
+            z = Math.ceil(extra/(numGroups-i))
+            extra -= Math.ceil(extra/(numGroups-i))
+        }else if(extra>0){
+            z = 1
+            extra--
+        }
+        groupsList.push(namesList.splice(0, size+z))
+    }
+
+    nameDoc = ''
+    groupsList.forEach(function(element, index){
+        addItem = 'Group '+(index+1)+': '
+        element.forEach(function(element2, index2){
+            addItem+=element2
+            if(element.length>index2+2){
+                addItem+=", "
+            }else if(element.length>index2+1){
+                addItem+=' and '
+            }else{
+                addItem+="."
+            }
+        })
+        nameDoc+=`<div class="groupItem">`+addItem+`</div>`
+    })
+    nameDoc += "<div class='divider'></div><div class='groupItem'>(Generated with seed "+seed+")</div>"
+    return nameDoc
+}
+
 function generateGroups(){
     names.forEach(function(el,ind){
-        var today = new Date()
-        customSeed = parseInt(document.getElementById("customSeed"+ind).value)
-        seed = (isNaN(customSeed) ? today.getDate() : customSeed)
-        namesList = []
-        el.namesList.forEach(function(element, index){
-            if(!document.getElementById('absent'+ind+'_'+index).checked){
-                namesList.push(element)
-            }
-        })
-        namesList = shuffle(namesList, seed)
-        //shuffled = ''
-        size = parseInt((document.getElementById('groupSize'+ind).value), 10)
-        if(isNaN(size)){
-            size=2
-        }
-        extra = namesList.length%size
-        numGroups = Math.floor(namesList.length/size)
-        namesList.forEach(function(element){
-            
-        })
-        groupsList=[]
-        for(i=0; i<numGroups; i++){
-            z = 0
-            if(extra/(numGroups-i)>1){
-                z = Math.ceil(extra/(numGroups-i))
-                extra -= Math.ceil(extra/(numGroups-i))
-            }else if(extra>0){
-                z = 1
-                extra--
-            }
-            groupsList.push(namesList.splice(0, size+z))
-        }
-
-        nameDoc = ''
-        groupsList.forEach(function(element, index){
-            addItem = 'Group '+(index+1)+': '
-            element.forEach(function(element2, index2){
-                addItem+=element2
-                if(element.length>index2+2){
-                    addItem+=", "
-                }else if(element.length>index2+1){
-                    addItem+=' and '
-                }else{
-                    addItem+="."
-                }
-            })
-            nameDoc+=`<div class="groupItem">`+addItem+`</div>`
-        })
-        nameDoc += "<div class='divider'></div><div class='groupItem'>(Generated with seed "+seed+")</div>"
-        document.getElementById('groups'+ind).innerHTML = nameDoc
+        document.getElementById('groups'+ind).innerHTML = createOneGroup(el, ind)
     })
 }
