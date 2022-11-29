ae724f2144c13f264379482fbd6aad50f782c855
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2022-11-29 15:39:08 -0500

    CT

--- memorization/main.js
+++ memorization/main.js
@@ -46,12 +46,22 @@ function retry(){
     showElement("gameArea")
 }
 
+function restart(){
+    gameStarted = false
+    currentWord = 0;
+    words=[]
+    correct = 0
+    showElement("startField")
+    hideElement("results")
+}
+
 function finishGame(){
     gameStarted = false
     console.log('done!')
     hideElement("gameArea")
     showElement("results")
-    document.getElementById("results").innerHTML += "<span>("+correct+"/"+(currentWord-1)+" correct)</span> <button onclick='retry()'>Retry</button>"
+    document.getElementById("results").innerHTML += "<span>("+correct+"/"+(currentWord-1)+" correct)</span> <button onclick='retry()'>Retry</button> <button onclick='restart()'>Go Back</button>"
+    document.getElementById("currentWord").style.color = "black"
 }
 
 function pressKey(key){
@@ -68,4 +78,42 @@ function pressKey(key){
     }
     if(currentWord >= words.length)
         finishGame()
+}
+
+function readTextFile(file)
+{
+    fetch(file)
+    .then(response => response.text())
+    .then(text => {
+        console.log(text)
+        return text.toString()
+    })
+}
+
+function replaceText(input){
+    newText = ''
+    switch(input){
+        case 'canterbury.txt':
+        newText = `Whan that Aprille with his shoures soote,
+The droghte of March hath perced to the roote,
+And bathed every veyne in swich licóur
+Of which vertú engendred is the flour;
+Whan Zephirus eek with his swete breeth
+Inspired hath in every holt and heeth
+The tendre croppes, and the yonge sonne
+Hath in the Ram his halfe cours y-ronne,
+And smale foweles maken melodye,
+That slepen al the nyght with open ye,
+So priketh hem Natúre in hir corages,
+Thanne longen folk to goon on pilgrimages,
+And palmeres for to seken straunge strondes,
+To ferne halwes, kowthe in sondry londes;
+And specially, from every shires ende
+Of Engelond, to Caunterbury they wende,
+The hooly blisful martir for to seke,
+That hem hath holpen whan that they were seeke.`
+        break
+    }
+    document.getElementById("toMemorize").value = newText
+
 }
