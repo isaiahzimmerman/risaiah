2bc92837295245e8e959572b0ae783128cc8093a
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2024-07-09 15:38:09 -0400

    Update questions.js

--- familyfeud/questions.js
+++ familyfeud/questions.js
@@ -1,22 +1,95 @@
 questions = [
+    //question 1
     {question: "What's an essential item to have in your bag during KidsFest?", answers: [
-        {answer: "WATER BOTTLE", points: 20, answered: false},
-        {answer: "GUN/WEAPON", points: 18, answered: false},
-        {answer: "MONEY", points: 16, answered: false},
-        // {answer: "answer 4", points: 14, answered: true},
-        {answer: "CANDY", points: 12, answered: false},
-        {answer: "BIKE", points: 10, answered: false},
-        // {answer: "answer 7", points: 10, answered: false},
+        {answer: "WATER", points: 45},
+        {answer: "BIBLE", points: 20},
+        {answer:"CANDY", points:7},
+        {answer:"SUNSCREEN", points:6},
+        {answer:"FOLDER", points:5},
+        {answer:"AQUAPHOR/CHAPSTICK", points:2},
     ]},
-    {question: "Name a House You Never Want to Be In", answers: [
-        {answer: "HAUNTED HOUSE", points: 27},
-        {answer: "JAIL/BIG HOUSE", points: 11},
-        {answer:"DOGHOUSE", points:8},
-        {answer:"DRUG HOUSE", points:7},
-        {answer:"SMALL HOUSE", points:7},
-        {answer:"GLASS HOUSE", points:6},
-        {answer:"CAT HOUSE", points:5},
-        {answer:"OUTHOUSE LOL THIS IS FUNNY", points:5},
+
+    //question 2
+    {question: "What's something that you never want a camper to approach you and say?", answers: [
+        {answer: "KF HATE", points: 17},
+        {answer: "VOMIT", points: 14},
+        {answer:"BROKE A BONE", points:13},
+        {answer:"PEED/POOPED PANTS", points:10},
+        {answer:"RIZZ/ASKING OUT", points:8},
+        {answer:"BLEEDING", points:5},
+        {answer:"I'M BORED", points:5},
+        {answer:"SWEARING", points:5},
+    ]},
+
+    //question 3
+    {question: "What's the weirdest food a camper could bring in for lunch?", answers: [
+        {answer: "FISH", points: 18},
+        {answer: "SUSHI", points: 8},
+        {answer:"VEGETABLES", points:7},
+        {answer:"SOUP", points:7},
+        {answer:"OCTOPUS", points:7},
+        {answer:"EGGS/FISH EGGS", points:7},
+        {answer:"FROG LEGS", points:3},
+        {answer:"PEANUT BUTTER", points:3},
+    ]},
+
+    //question 4
+    {question: "If you could pick any color or design for a KidsFest t-shirt, what would it be?", answers: [
+        {answer: "PINK", points: 30},
+        {answer: "BLUE", points: 16},
+        {answer:"TIE-DYE", points:11},
+        {answer:"PURPLE", points:11},
+        {answer:"BLACK", points:3},
+        {answer:"TEAL", points:3},
+        {answer:"GREEN", points:2},
+        {answer:"CAMO", points:2},
+    ]},
+    
+    //question 5
+    {question: "What would you be doing if you weren't doing KidsFest?", answers: [
+        {answer: "WORKING", points: 35},
+        {answer: "SLEEPING", points: 17},
+        {answer:"SPORTS/EXERCISE", points:12},
+        {answer:"NOTHING", points:11},
+        {answer:"FRIENDS/FAMILY", points:5},
+        {answer:"VACATION/TANNING", points:3},
+        {answer:"VIDEO GAMES/TV", points:2},
+
+    ]},
+    // question 6
+    {question: "What is the most fun activity at Free Choice", answers: [
+        {answer: "GAGA BALL", points: 36},
+        {answer: "BOUNCY HOUSE/RACE", points: 10},
+        {answer:"PAINTBALL", points:9},
+        {answer:"ARCHERY", points:6},
+        {answer:"BASKETBALL", points:5},
+        {answer:"CRAFTS", points:5},
+        {answer:"WATER", points:5},
+        {answer:"LEADERFESTING/CHILLING", points:3},
+
+    ]},
+
+    // question 7
+    {question: "What is a reason a Camper would be kicked out of KidsFest?", answers: [
+        {answer: "VIOLENCE", points: 38},
+        {answer: "SWEARING", points: 10},
+        {answer:"BULLYING/BEING MEAN", points:7},
+        {answer:"WEAPON", points:6},
+        {answer:"DISRESPECT", points:4},
+        {answer:"BRINGING DRUGS/CONTRABAND", points:3},
+        {answer:"STEALING", points:3},
+        {answer:"RUNNING AWAY", points:2},
+
+    ]},
+
+    // question 8
+    {question: "What is the most broken rule by leaders?", answers: [
+        {answer: "LEADERFESTING", points: 29},
+        {answer: "USING PHONE", points: 25},
+        {answer:"NEGLECT/LOSING CAMPER", points:10},
+        {answer:"SWEARING", points:7},
+        {answer:"MIDDLE SCHOOL SERVE ABUSE", points:2},
+        {answer:"PURPLING", points:1},
 
     ]},
 ]
